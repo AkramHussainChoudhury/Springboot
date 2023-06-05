54dfa66c357f64e5f7430972e94e07034b056bd6
AkramHussainChoudhury <akramchoudhury90@gmail.com>
2023-06-06 06:16:48 +0800

    "Springboot basics"

---
+++ src/test/java/com/akram/springbootbasics/SpringbootbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akram.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
